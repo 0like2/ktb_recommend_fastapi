0e09c576536325a686835369fd7b503605a1f2ef
0like2 <dudfkr236@gmail.com>
2024-11-19 09:25:27 +0900

    feature : 기존 base logic 구현

---
+++ src/main.py
@@ -0,0 +1,23 @@
+import os
+
+from dotenv import load_dotenv
+from fastapi import FastAPI
+
+# Routers
+from src.profile.router import router as profile_router
+from proposal.router import router as proposal_router
+from recommendation.router import router as recommendation_router
+
+# config
+load_dotenv()
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+# router
+app = FastAPI()
+app.include_router(profile_router)
+app.include_router(proposal_router)
+app.include_router(recommendation_router)
+
+@app.get("/")
+def root():
+    return {"message": "Welcome to Project Joing AI Api Server!"}
